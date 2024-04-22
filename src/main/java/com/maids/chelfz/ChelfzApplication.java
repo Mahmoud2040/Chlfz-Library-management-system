c78faec35150eea5baa242a8ab9d73552fd3a358
Mahmoud Sharaf <mahmoud.elmasry7018@gmail.com>
2024-04-23 01:28:15 +0200

    Initial project setup and dependency configuration

---
+++ src/main/java/com/maids/chelfz/ChelfzApplication.java
@@ -0,0 +1,13 @@
+package com.maids.chelfz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChelfzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChelfzApplication.class, args);
+    }
+
+}
